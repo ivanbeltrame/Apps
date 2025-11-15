da29d3b1eb93339701de84520e001bd784ed8067
Ivan Beltrame <87547424+Imperatore1003@users.noreply.github.com>
2025-11-15 19:11:57 +0100

    Storing default values in js and working on localStorage support

--- pomodoro/main.js
+++ pomodoro/main.js
@@ -1,5 +1,5 @@
 let timer;
-let timerType = "pomodoro";
+let timerType = "pomodoro"; // Starting position
 let iteration = 1;
 let timerSpan = document.getElementById("timer");
 let pomodoroTimerSetting = document.getElementById("pomodoroDuration");
@@ -7,14 +7,14 @@ let shortTimerSetting = document.getElementById("shortDuration");
 let longTimerSetting = document.getElementById("longDuration");
 let longIntervalSetting = document.getElementById("longInterval");
 
-let pomodoroTime;
-let shortTime;
-let longTime;
-let longBreakIterations;
+// Default settings
+let pomodoroTime = 60 * 25;
+let shortTime = 60 * 5;
+let longTime = 60 * 15;
+let longBreakIterations = 4;
 
 function startTimer() {
     function updateTimer() {
-        // console.log(seconds);
         timerSpan.innerHTML = formatSeconds(seconds);
         if (seconds == 0) {
             if (timerType == "pomodoro") {
@@ -55,6 +55,11 @@ function updateSettings() {
     longTime = longTimerSetting.value * 60;
     longBreakIterations = longIntervalSetting.value;
 
+    // localStorage.setItem("pomodoroTime", pomodoroTime / 60);
+    // localStorage.setItem("shortTime", shortTime / 60);
+    // localStorage.setItem("longTime", longTime / 60);
+    // localStorage.setItem("longBreakIterations", longBreakIterations);
+
     if (timerType == "pomodoro") timerSpan.innerHTML = formatSeconds(pomodoroTime);
     else if (timerType == "short") timerSpan.innerHTML = formatSeconds(shortTime);
     else timerSpan.innerHTML = formatSeconds(longTime);
@@ -69,8 +74,21 @@ function formatSeconds(total) {
 document.querySelectorAll('input[name="timerType"]').forEach(radio => {
     radio.addEventListener('change', () => {
         updateTimerType(radio.value);
-        console.log("changed")
     });
 });
 
-updateSettings();
+// let lsPomodoroTime = localStorage.getItem("pomodoroTime");
+// if (lsPomodoroTime) pomodoroTime = lsPomodoroTime * 60;
+// let lsShortTime = localStorage.getItem("shortTime");
+// if (lsShortTime) pomodoroTime = lsShortTime * 60;
+// let lsLongTime = localStorage.getItem("longTime");
+// if (lsLongTime) pomodoroTime = lsLongTime * 60;
+// let lsLongIterationsTime = localStorage.getItem("longBreakIterations");
+// if (lsLongIterationsTime) pomodoroTime = lsLongIterationsTime;
+// updateSettings();
+
+// Reset modal settings -> after refreshing the page
+pomodoroTimerSetting.value = pomodoroTime / 60;
+shortTimerSetting.value = shortTime / 60;
+longTimerSetting.value = longTime / 60;
+longIntervalSetting.value = longBreakIterations;
